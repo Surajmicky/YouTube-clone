b88d0889495b10b8e22db36b86c82b636df77df4
Suraj kumar <90993575+Surajmicky@users.noreply.github.com>
2022-01-07 22:30:22 +0530

    added youtube api
    added youtube api for display div

--- components/navbar.js
+++ components/navbar.js
@@ -8,7 +8,7 @@ function navbar() {
     <div id="logodiv">
         <img  style="height: 20px;" src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b8/YouTube_Logo_2017.svg/2560px-YouTube_Logo_2017.svg.png" alt="">
     </div >
-   <div id="searchdiv" ><input id="searchbox" type="text" placeholder="  search">
+   <div id="searchdiv" ><input id="searchbox" type="text" placeholder=" Search">
    <div id="searchicon"><img class="navimg"  src="/youtube_app/icons/search.png" alt=""></div>
 </div>
 <div><img  style="height: 20px; padding-bottom: 5px;"  class="navimg" src="/youtube_app/icons/mic.png" alt=""></div>
---
+++ scripts/showData.js
@@ -0,0 +1,77 @@
+// youtube videos details and for each div script
+async function getData(url){
+
+    try{
+
+        let result= await fetch(url);
+
+        let data = await result.json();
+
+        return data;
+
+    }
+
+    catch(error){
+
+        console.log(error)
+    }
+}
+
+
+function appendData(data,location){
+
+    data.forEach(element => {
+
+        let { id:{videoId},snippet:{channelTitle,description,thumbnails:{high:{url}},title}} =element;
+
+        let videobox= document.createElement('div');
+
+        videobox.setAttribute('id','videobox');
+
+        let thumbnail_div = document.createElement('div');
+
+        let detail_div = document.createElement('div');
+
+        thumbnail_div.setAttribute('id','thumbnail_div');
+
+        detail_div.setAttribute('id','detail_div');
+
+        let thumbnail= document.createElement('img');
+
+        thumbnail.src=url;
+
+        let name= document.createElement('p');
+
+        name.innerText=title;
+
+        thumbnail.setAttribute('id','thumbnail');
+
+        name.setAttribute('id','name');
+
+        thumbnail_div.append(thumbnail);
+
+        let video_description = document.createElement('p');
+
+        video_description.innerText=description;
+
+        video_description.setAttribute('id','description')
+
+        let channel_name = document.createElement('p');
+
+        channel_name.innerText= channelTitle;
+
+        channel_name.setAttribute('id','channel_name')
+
+        detail_div.append(name,channel_name,video_description);
+
+        videobox.append(thumbnail_div,detail_div)
+
+        location.append(videobox);
+         
+    });
+}
+export {getData,appendData}
+
+//https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&regionCode=US&key=[YOUR_API_KEY]
+
+
