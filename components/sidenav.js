7135a6e4a5f845ecbda6955bfbe3d5056e724a0f
Suraj kumar <90993575+Surajmicky@users.noreply.github.com>
2022-01-07 22:56:25 +0530

    Update sidenav.js

--- components/sidenav.js
+++ components/sidenav.js
@@ -4,16 +4,16 @@ function sidenav() {
   return `
     
         <div id="sidenav_options">
-            <div class="sidenav_option"div><div class="sideicondiv"><img class=sideicon src="/youtube_app/icons/home.png" alt=""></div><div class="sidenavdiv1">Home </div></div>
-            <div class="sidenav_option"div><div class="sideicondiv"><img class=sideicon src="/youtube_app/icons/compass.png" alt=""></div><div class="sidenavdiv1">Explore </div></div>
-            <div class="sidenav_option"div><div class="sideicondiv"><img class=sideicon src="/youtube_app/icons/subscribe.png" alt=""></div><div class="sidenavdiv1">Subscriptions </div></div>
+            <div class="sidenav_option"div><div class="sideicondiv"><img class=sideicon src="./icons/home.png" alt=""></div><div class="sidenavdiv1">Home </div></div>
+            <div class="sidenav_option"div><div class="sideicondiv"><img class=sideicon src="./icons/compass.png" alt=""></div><div class="sidenavdiv1">Explore </div></div>
+            <div class="sidenav_option"div><div class="sideicondiv"><img class=sideicon src="./icons/subscribe.png" alt=""></div><div class="sidenavdiv1">Subscriptions </div></div>
             <div id="betweendiv"></div> 
         <div class="sidenav_option"div><div class="sideicondiv"><img class=sideicon src="./icons/library.png" alt=""></div><div class="sidenavdiv1">Library </div></div>
-             <div class="sidenav_option"div><div class="sideicondiv"><img class=sideicon src="/youtube_app/icons/history.png" alt=""></div><div class="sidenavdiv1">History </div></div>
-        <div class="sidenav_option"div><div class="sideicondiv"><img class=sideicon src="/youtube_app/icons/youtube.png" alt=""></div><div class="sidenavdiv1">Your Videos </div></div>
-             <div class="sidenav_option"div><div class="sideicondiv"><img class=sideicon src="/youtube_app/icons/wall-clock.png" alt=""></div><div class="sidenavdiv1">Watch Later </div></div>
-        <div class="sidenav_option"div><div class="sideicondiv"><img class=sideicon src="/youtube_app/icons/flag-or-text-lines-with-a-video.png" alt=""></div><div class="sidenavdiv1">Saved Videos</div></div>
-        <div class="sidenav_option"div><div class="sideicondiv"><img class=sideicon src="/youtube_app/icons/down-arrow.png" alt=""></div><div class="sidenavdiv1">Show More </div></div>
+             <div class="sidenav_option"div><div class="sideicondiv"><img class=sideicon src="./icons/history.png" alt=""></div><div class="sidenavdiv1">History </div></div>
+        <div class="sidenav_option"div><div class="sideicondiv"><img class=sideicon src="./icons/youtube.png" alt=""></div><div class="sidenavdiv1">Your Videos </div></div>
+             <div class="sidenav_option"div><div class="sideicondiv"><img class=sideicon src="./icons/wall-clock.png" alt=""></div><div class="sidenavdiv1">Watch Later </div></div>
+        <div class="sidenav_option"div><div class="sideicondiv"><img class=sideicon src="./icons/flag-or-text-lines-with-a-video.png" alt=""></div><div class="sidenavdiv1">Saved Videos</div></div>
+        <div class="sidenav_option"div><div class="sideicondiv"><img class=sideicon src="./icons/down-arrow.png" alt=""></div><div class="sidenavdiv1">Show More </div></div>
             
         </div>
         <div>
